90c37b7b7d5ec5364fabdd7c0f5653fe3af24ae2
UNovak <urbanwritescode@gmail.com>
2024-03-21 21:20:43 +0100

    Implemented shadeing behind pop-up, fixed pop-up height

--- src/components/WalletPopUp/WalletPopUp.tsx
+++ src/components/WalletPopUp/WalletPopUp.tsx
@@ -17,17 +17,25 @@ const WalletPopUp = ({ isVisible }: WalletPopUpProps) => {
   }, []);
 
   return isVisible ? (
-    <div className='WalletPopUp'>
-      <RainbowBorder backgroundColor={'#ffffff'} borderRadius={25} width={10} />
-      <h1>Select the wallet to connect</h1>
-      <div className='WalletPopUp__icons'>
-        <Icons.PhantomIcon width={169} height={141} />
-        <Icons.MetaMaskIcon width={153} height={141} />
+    <>
+      <div className='WalletPopUp'>
+        <RainbowBorder
+          backgroundColor={'#ffffff'}
+          borderRadius={25}
+          width={10}
+        />
+        <h1>Select the wallet to connect</h1>
+        <div className='WalletPopUp__icons'>
+          <Icons.PhantomIcon width={169} height={141} />
+          <Icons.MetaMaskIcon width={153} height={141} />
+        </div>
+
+        <Button style='primary' label='Use Phantom' borderWidth={6} />
+        <Button style='primary' label='Use MetaMask' borderWidth={6} />
       </div>
 
-      <Button style='primary' label='Use Phantom' borderWidth={6} />
-      <Button style='primary' label='Use MetaMask' borderWidth={6} />
-    </div>
+      <div className='shader' />
+    </>
   ) : null;
 };
 

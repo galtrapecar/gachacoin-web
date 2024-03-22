3f982a58619b7336571cfdbc0dcd7a21e153f8ce
UNovak <urbanwritescode@gmail.com>
2024-03-22 16:14:15 +0100

    Implemented catalogueCard

---
+++ src/components/CatalogueCard/CatalogueCard.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import RainbowBorder from '../RainbowBorder/RainbowBorder';
+import Icons from '../../assets/icons';
+
+export type CatalogueCardProps = {
+  name: string;
+  link?: string;
+  serial: number;
+  collection: string;
+};
+
+const CatalogueCard: React.FC<CatalogueCardProps> = ({
+  name,
+  link,
+  serial,
+  collection,
+}) => {
+  return (
+    <div className='card'>
+      <div className='card__image__container'>
+        <img
+          className='card__image'
+          src={require('../../assets/images/placeholder.jpeg')}
+          alt=''
+        />
+        {/* <RainbowBorder backgroundColor='#ffffff' width={3} borderRadius={17} /> */}
+      </div>
+      <div className='card__text__container'>
+        <p className='card__title'>{name}</p>
+        <p className='card__serial'>{serial}</p>
+      </div>
+      <div className='card__icon_container'>
+        <Icons.CollectionIcon width={27} height={27} />
+      </div>
+    </div>
+  );
+};
+
+export default CatalogueCard;
+
+// FIXME: Rainbow image border
+// FIXME: Positioning of collection icon
+// FIXME: Dinamicly render image and collection icon
--- src/pages/catalogue/CataloguePage.tsx
+++ src/pages/catalogue/CataloguePage.tsx
@@ -1,14 +1,100 @@
-import { useRecoilValue } from "recoil";
-import PageHeading from "../../components/PageHeading/PageHeading";
-import { symbolAtom } from "../../state";
+import React from 'react';
+import { useRecoilValue } from 'recoil';
+import PageHeading from '../../components/PageHeading/PageHeading';
+import { symbolAtom } from '../../state';
+import CatalogueCard from '../../components/CatalogueCard/CatalogueCard';
 
 const CataloguePage = () => {
   const symbol = useRecoilValue(symbolAtom);
+
+  const cards = [
+    {
+      name: 'Card 1',
+      link: '../../assets/images/placeholder.jpeg',
+      serial: 123456,
+      collection: 'Collection A',
+    },
+    {
+      name: 'Card 2',
+      link: '../../assets/images/placeholder.jpeg',
+      serial: 789012,
+      collection: 'Collection B',
+    },
+    {
+      name: 'Card 2',
+      link: '../../assets/images/placeholder.jpeg',
+      serial: 789012,
+      collection: 'Collection B',
+    },
+    {
+      name: 'Card 2',
+      link: '../../assets/images/placeholder.jpeg',
+      serial: 789012,
+      collection: 'Collection B',
+    },
+    {
+      name: 'Card 1',
+      link: '../../assets/images/placeholder.jpeg',
+      serial: 123456,
+      collection: 'Collection A',
+    },
+    {
+      name: 'Card 2',
+      link: '../../assets/images/placeholder.jpeg',
+      serial: 789012,
+      collection: 'Collection B',
+    },
+    {
+      name: 'Card 2',
+      link: '../../assets/images/placeholder.jpeg',
+      serial: 789012,
+      collection: 'Collection B',
+    },
+    {
+      name: 'Card 2',
+      link: '../../assets/images/placeholder.jpeg',
+      serial: 789012,
+      collection: 'Collection B',
+    },
+    {
+      name: 'Card 1',
+      link: '../../assets/images/placeholder.jpeg',
+      serial: 123456,
+      collection: 'Collection A',
+    },
+    {
+      name: 'Card 2',
+      link: '../../assets/images/placeholder.jpeg',
+      serial: 789012,
+      collection: 'Collection B',
+    },
+    {
+      name: 'Card 2',
+      link: '../../assets/images/placeholder.jpeg',
+      serial: 789012,
+      collection: 'Collection B',
+    },
+    {
+      name: 'Card 2',
+      link: '../../assets/images/placeholder.jpeg',
+      serial: 789012,
+      collection: 'Collection B',
+    },
+    // Add more card objects as needed
+  ];
+
   return (
-    <div className="CataloguePage">
-      <PageHeading title={"Catalogue"} symbol={symbol} subtitle={"カタログ"} />
+    <div className='CataloguePage'>
+      <PageHeading title={'Catalogue'} symbol={symbol} subtitle={'カタログ'} />
+      <div className='cards'>
+        {cards.map((card, index) => (
+          <CatalogueCard key={index} {...card} />
+        ))}
+      </div>
     </div>
   );
 };
 
 export default CataloguePage;
+
+// FIXME: Extanding background color if there is more than 2 rows of cards

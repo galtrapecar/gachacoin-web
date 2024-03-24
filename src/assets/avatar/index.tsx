06c65275169d94e6cdd93c0631740a46ace65567
gal trapecar <trapecar.gal@gmail.com>
2024-03-24 11:11:49 +0100

    adds avatar full icon

--- src/assets/avatar/index.tsx
+++ src/assets/avatar/index.tsx
@@ -1,3 +1,4 @@
+import { ReactComponent as Avatar } from './avatar.svg';
 import { ReactComponent as ArmLeft } from './arm_left.svg';
 import { ReactComponent as ArmRight } from './arm_righ.svg';
 import { ReactComponent as Body } from './body.svg';
@@ -6,6 +7,7 @@ import { ReactComponent as FootRight } from './foot_right.svg';
 import { ReactComponent as Head } from './head.svg';
 
 export const AvatarIcons = {
+  Avatar,
   ArmLeft,
   ArmRight,
   Body,

83a14f9bae2a3395ce8e8227401a4d1cdd53912b
gal trapecar <trapecar.gal@gmail.com>
2024-03-24 12:48:51 +0100

    adds avatar icons to page

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -3,6 +3,7 @@ import { ReactComponent as CollectionIcon } from './Collection.svg';
 import { ReactComponent as GearIcon } from './Gear.svg';
 import { ReactComponent as MetaMaskIcon } from './MetaMask.svg';
 import { ReactComponent as PhantomIcon } from './Phantom.svg';
+import { ReactComponent as PlusCircleIcon } from './PlusCircle.svg';
 
 const Icons = {
   CheckmarkIcon,
@@ -10,6 +11,7 @@ const Icons = {
   GearIcon,
   MetaMaskIcon,
   PhantomIcon,
+  PlusCircleIcon,
 };
 
 export default Icons;
--- src/pages/avatar/AvatarPage.tsx
+++ src/pages/avatar/AvatarPage.tsx
@@ -1,12 +1,36 @@
 import { useRecoilValue } from 'recoil';
 import PageHeading from '../../components/PageHeading/PageHeading';
 import { symbolAtom } from '../../state';
+import AvatarIcons from '../../assets/avatar';
+import Icons from '../../assets/icons';
 
 const AvatarPage = () => {
   const symbol = useRecoilValue(symbolAtom);
   return (
     <div className="AvatarPage">
-      <PageHeading title={'Avatar'} symbol={symbol} subtitle={'アバター'} />
+      <PageHeading title={'GachaMon'} symbol={symbol} subtitle={'ガチャモン'} />
+      <div className="AvatarPage__avatar">
+        <div className="AvatarPage__avatar__head">
+          <AvatarIcons.Head strokeDasharray={'10 10'} />
+        </div>
+        <div className="AvatarPage__avatar__body">
+          <div className="AvatarPage__avatar__arm--left">
+            <AvatarIcons.ArmLeft strokeDasharray={'10 10'} />
+          </div>
+          <AvatarIcons.Body className='AvatarPage__avatar__body--body' strokeDasharray={'10 10'} />
+          <div className="AvatarPage__avatar__arm--right">
+            <AvatarIcons.ArmRight strokeDasharray={'10 10'} />
+          </div>
+        </div>
+        <div className="AvatarPage__avatar__feet">
+          <div className="AvatarPage__avatar__foot--left">
+            <AvatarIcons.FootLeft strokeDasharray={'10 10'} />
+          </div>
+          <div className="AvatarPage__avatar__foot--right">
+            <AvatarIcons.FootRight strokeDasharray={'10 10'} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

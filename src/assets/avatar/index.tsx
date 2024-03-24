ed3089e8be85ad9f43e420232d3b6536878a7e9e
gal trapecar <trapecar.gal@gmail.com>
2024-03-24 11:09:39 +0100

    adds avatar icons

---
+++ src/assets/avatar/index.tsx
@@ -0,0 +1,15 @@
+import { ReactComponent as ArmLeft } from './arm_left.svg';
+import { ReactComponent as ArmRight } from './arm_righ.svg';
+import { ReactComponent as Body } from './body.svg';
+import { ReactComponent as FootLeft } from './foot_left.svg';
+import { ReactComponent as FootRight } from './foot_right.svg';
+import { ReactComponent as Head } from './head.svg';
+
+export const AvatarIcons = {
+  ArmLeft,
+  ArmRight,
+  Body,
+  FootLeft,
+  FootRight,
+  Head,
+};

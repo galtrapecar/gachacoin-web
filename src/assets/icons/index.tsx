3f24605f2fbddb4feccb388e6d56320bf4d6177f
UNovak <urbanwritescode@gmail.com>
2024-03-24 02:39:13 +0100

    Imported ETH icon

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -2,12 +2,14 @@ import { ReactComponent as GearIcon } from './Gear.svg';
 import { ReactComponent as MetaMaskIcon } from './MetaMask.svg';
 import { ReactComponent as PhantomIcon } from './Phantom.svg';
 import { ReactComponent as CollectionIcon } from './Collection.svg';
+import { ReactComponent as EthIcon } from './Eth.svg';
 
 const Icons = {
   GearIcon,
   MetaMaskIcon,
   PhantomIcon,
   CollectionIcon,
+  EthIcon
 };
 
 export default Icons;

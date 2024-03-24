8a7098a44385e70fb7dfc3b24423e34acbe7003b
UNovak <urbanwritescode@gmail.com>
2024-03-24 02:33:30 +0100

    Implemented MarketplaceCard and Marketplace view

--- src/pages/marketplace/Marketplace.tsx
+++ src/pages/marketplace/Marketplace.tsx
@@ -1,9 +1,74 @@
-import { useRecoilValue } from 'recoil';
-import PageHeading from '../../components/PageHeading/PageHeading';
 import { symbolAtom } from '../../state';
+import { useRecoilValue } from 'recoil';
+import MarketplaceCard from '../../components/MarketplaceCard/MarketplaceCard';
+import PageHeading from '../../components/PageHeading/PageHeading';
 
 const MarketplacePage = () => {
   const symbol = useRecoilValue(symbolAtom);
+
+  const cards = [
+    {
+      name: 'Part Name',
+      price: 0.3,
+      seller: 'NoName',
+      serial: 3,
+    },
+    {
+      name: 'Part Name',
+      price: 0.3,
+      seller: 'NoName',
+      serial: 3333,
+    },
+    {
+      name: 'Part Name',
+      price: 0.3,
+      seller: 'NoName',
+      serial: 333333333,
+    },
+    {
+      name: 'Part Name',
+      price: 0.3,
+      seller: 'NoName',
+      serial: 3333,
+    },
+    {
+      name: 'Part Name',
+      price: 0.3,
+      seller: 'NoName',
+      serial: 3333,
+    },
+    {
+      name: 'Part Name',
+      price: 0.3,
+      seller: 'NoName',
+      serial: 3333,
+    },
+    {
+      name: 'Part Name',
+      price: 0.3,
+      seller: 'NoName',
+      serial: 3333,
+    },
+    {
+      name: 'Part Name',
+      price: 0.3,
+      seller: 'NoName',
+      serial: 3333,
+    },
+    {
+      name: 'Part Name',
+      price: 0.3,
+      seller: 'NoName',
+      serial: 3333,
+    },
+    {
+      name: 'Part Name',
+      price: 0.3,
+      seller: 'NoName',
+      serial: 3333,
+    },
+  ];
+
   return (
     <div className="MarketplacePage">
       <PageHeading
@@ -11,6 +76,11 @@ const MarketplacePage = () => {
         symbol={symbol}
         subtitle={'カタログ'}
       />
+      <div className="Marketplace__cards">
+        {cards.map((card, index) => (
+          <MarketplaceCard key={index} {...card} />
+        ))}
+      </div>
     </div>
   );
 };

672b44e4db8018436e99be8fa75933b5d374b576
gal trapecar <trapecar.gal@gmail.com>
2024-03-25 13:51:21 +0100

    adds text and gacha machine

--- src/pages/landing/LadingPage.tsx
+++ src/pages/landing/LadingPage.tsx
@@ -1,20 +1,29 @@
 import useSpeedLines from '../../hooks/useSpeedLines';
 import Button from '../../components/Button/Button';
 import SpinPopUp from '../../components/SpinPopUp/SpinPopUp';
-import { spinPopupStatusAtom } from '../../state';
-import { useRecoilState } from 'recoil';
+import { AppSymbol, spinPopupStatusAtom, symbolAtom } from '../../state';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { GachaPon } from '../../assets/images';
 
 const LandingPage = () => {
   const Canvas = useSpeedLines();
+  const symbol = useRecoilValue(symbolAtom);
   const [popupStatus, setPopupStatus] = useRecoilState(spinPopupStatusAtom);
 
   const displayPopup = () => {
     setPopupStatus(true);
   };
 
+  const character = symbol === AppSymbol.Heart ? AppSymbol.Heart : '!';
+
   return (
     <div className="LandingPage">
       <div className="LandingPage__background">{Canvas}</div>
+      <img className='LandingPage__gachapon' src={GachaPon.Image} />
+      <div className="LandingPage__textOverlay">
+        <div className="LandingPage__textOverlay__text">ウィンビッグ{Array(1).fill(character)}</div>
+        <div className="LandingPage__textOverlay__text">{`スピン${Array(3).fill(character).join('')}`}</div>
+      </div>
       <div className="LandingPage__about">
         <div className="LandingPage__about__fold" />
       </div>

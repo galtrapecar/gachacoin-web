7175b3b0fec091aa5f1e1966728a9ea6e2c45140
gal trapecar <trapecar.gal@gmail.com>
2024-04-08 00:08:33 +0200

    adds balance field and chain switching

--- src/components/AccountField/AccountField.tsx
+++ src/components/AccountField/AccountField.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useRef } from 'react';
 import jazzicon from '@metamask/jazzicon';
+import { useSDK } from '@metamask/sdk-react';
+import Web3 from 'web3';
 
 type AccountFieldProps = {
   account: string;
@@ -7,6 +9,7 @@ type AccountFieldProps = {
 };
 
 const AccountField = ({ account, ethBalance }: AccountFieldProps) => {
+  const { chainId, provider } = useSDK();
   const iconRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
@@ -22,16 +25,70 @@ const AccountField = ({ account, ethBalance }: AccountFieldProps) => {
     }
   }, [account, iconRef]);
 
+  const isPolygonMumbai = () => {
+    const chainIdInt = Web3.utils.hexToNumber(chainId || '0x0');
+    if (chainIdInt !== 80001) return false;
+    return true;
+  };
+
+  const addPolygonChain = async () => {
+    if (!provider) return;
+    try {
+      await provider.request({
+        method: 'wallet_addEthereumChain',
+        params: [
+          {
+            chainName: 'Polygon Mumbai',
+            chainId: Web3.utils.toHex(80001),
+            nativeCurrency: { name: 'MATIC', decimals: 18, symbol: 'MATIC' },
+            rpcUrls: ['https://polygon-mumbai-bor-rpc.publicnode.com'],
+          },
+        ],
+      });
+    } catch (error) {
+      console.warn('Rejected chain add...', error);
+    }
+  };
+
+  const switchToPolygon = async () => {
+    if (!provider) return;
+    try {
+      await provider.request({
+        method: 'wallet_switchEthereumChain',
+        params: [{ chainId: Web3.utils.toHex(80001) }],
+      });
+    } catch (err: any) {
+      // This error code indicates that the chain has not been added to MetaMask
+      if (err.code === 4902) {
+        addPolygonChain();
+      }
+    }
+  };
+
   return (
     <div className="AccountField">
       <div className="AccountField__details">
-        <div className="AccountField__details__balance">{ethBalance} ETH</div>
-        <div className="AccountField__details__account">
-          {[
-            account.slice(0, 5),
-            account.slice(account.length - 3, account.length),
-          ].join('...')}
-        </div>
+        {isPolygonMumbai() && (
+          <>
+            <div className="AccountField__details__balance">
+              {String(Web3.utils.fromWei(Web3.utils.hexToNumber(ethBalance || '0x0'), 'ether'))} MATIC
+            </div>
+            <div className="AccountField__details__account">
+              {[
+                account.slice(0, 5),
+                account.slice(account.length - 3, account.length),
+              ].join('...')}
+            </div>
+          </>
+        )}
+        {!isPolygonMumbai() && (
+          <div
+            className="AccountField__details__button"
+            onClick={() => switchToPolygon()}
+          >
+            Switch to Polygon
+          </div>
+        )}
       </div>
       <div className="AccountField__icon" ref={iconRef} />
     </div>

29e06eb443f66c98173eb2d61c4339f60b035f12
UNovak <urbanwritescode@gmail.com>
2024-04-07 18:16:42 +0200

    AboutPage content  width adjusted for bigger screen to max 1000px

--- src/pages/about/AboutPage.tsx
+++ src/pages/about/AboutPage.tsx
@@ -70,52 +70,56 @@ const AboutPage = () => {
           icon={<Icons.Questionmark width={24} height={24} />}
         />
       </div>
-      <div className="AboutPage__content">
-        <Icons.Coin width={125} height={125} className="__coin__icon" />
-        <p className="__introduction__text">
-          With Gocha, users collect digital Kawaii collectables that can be
-          equipped to their avatars! Each Gocha item is released in limited
-          amounts, influencing its value. Players can acquire new items by
-          either trading with other players or spinning the Gocha-Slot-Machine.
-          In both cases different amount of the native GochaCoin crypt currency
-          are required.
-        </p>
-        <p className="__introduction__text">
-          Gocha is one of the world's first blockchain digitally wearable
-          collectables. Blockchain is the technology that makes things like
-          Bitcoin possible. While Gocha isn't a digital currency, it does offer
-          the same security: Each Gocha is available in limited supply and 100%
-          owned by you. It cannot be replicated, taken away, or destroyed.
-        </p>
-        <div className="__title__wrapper">
-          <h5 className="__title">GochaCoin Team</h5>
-        </div>
-        <div className="__teamMember__cards__container">
-          {teamMembers.map((member, index) => (
-            <TeamMemberCard key={index} {...member} />
-          ))}
-        </div>
-        <div className="__title__wrapper">
-          <h5 className="__title">The Gocha Manifesto</h5>
-        </div>
+      <div className="AboutPage__content__container">
+        <div className="AboutPage__content">
+          <Icons.Coin width={125} height={125} className="__coin__icon" />
+          <p className="__introduction__text">
+            With Gocha, users collect digital Kawaii collectables that can be
+            equipped to their avatars! Each Gocha item is released in limited
+            amounts, influencing its value. Players can acquire new items by
+            either trading with other players or spinning the
+            Gocha-Slot-Machine. In both cases different amount of the native
+            GochaCoin crypt currency are required.
+          </p>
+          <p className="__introduction__text">
+            Gocha is one of the world's first blockchain digitally wearable
+            collectables. Blockchain is the technology that makes things like
+            Bitcoin possible. While Gocha isn't a digital currency, it does
+            offer the same security: Each Gocha is available in limited supply
+            and 100% owned by you. It cannot be replicated, taken away, or
+            destroyed.
+          </p>
+          <div className="__title__wrapper">
+            <h5 className="__title">GochaCoin Team</h5>
+          </div>
+          <div className="__teamMember__cards__container">
+            {teamMembers.map((member, index) => (
+              <TeamMemberCard key={index} {...member} />
+            ))}
+          </div>
+          <div className="__title__wrapper">
+            <h5 className="__title">The Gocha Manifesto</h5>
+          </div>
 
-        <div className="__manifesto__container">
-          <p className="__column first">
-            What we believe in, what we stand for, and why we're building Gocha.
-            The future is exciting. And we believe that blockchain is the
-            future—but blockchain is about as approachable as a bunch of ones
-            and zeroes. We want a future for everyone, not one exclusive to
-            Bitcoin miners, VCs, ICOs, and other equally fun acronyms.
-          </p>
-          <p className="__column second">
-            So why Digital weeaboo Collectables? Cats are impossible to
-            understand. They're ambassadors for pharaohs, memes, and your mom's
-            facebook page. They don't discriminate; they despise everyone
-            equally
-            <h5 className="__ending">The future is Gocha</h5>
-          </p>
+          <div className="__manifesto__container">
+            <p className="__column first">
+              What we believe in, what we stand for, and why we're building
+              Gocha. The future is exciting. And we believe that blockchain is
+              the future—but blockchain is about as approachable as a bunch of
+              ones and zeroes. We want a future for everyone, not one exclusive
+              to Bitcoin miners, VCs, ICOs, and other equally fun acronyms.
+            </p>
+            <p className="__column second">
+              So why Digital weeaboo Collectables? Cats are impossible to
+              understand. They're ambassadors for pharaohs, memes, and your
+              mom's facebook page. They don't discriminate; they despise
+              everyone equally
+              <h5 className="__ending">The future is Gocha</h5>
+            </p>
+          </div>
         </div>
       </div>
+
       <div className="AboutPage__footer" />
     </div>
   );
@@ -123,6 +127,6 @@ const AboutPage = () => {
 
 export default AboutPage;
 
-// FIXME: The Gocha manifesto columns not equal width
 // TODO: Add navigation buttons functionality
 // TODO: Update footer with content
+// FIXME: Title should stay aligned with left most card
